8e87427c948a24283581ffc3b009ceb25a8a84f9
Rohit Mondal <mondalrohit903@gmail.com>
2023-02-15 12:47:16 +0530

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,72 @@
+import { Container, Row, Col, Tab, Nav, Button } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/projImg1.png";
+import projImg2 from "../assets/img/projImg2.png";
+import projImg3 from "../assets/img/projImg3.png";
+import projImg4 from "../assets/img/projImg4.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Voice Assistant",
+      description: "Made using python to perform very basic task",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Flappy Bird",
+      description: "The android 5 easter egg made using python",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Library Management System",
+      description: "Helps maintaining record of books issued and returned",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Healthy Programmer",
+      description: "Reminders in a day to drink water, perform exercises to stay fit and healthy",
+      imgUrl: projImg4,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>My projects include a voice Assistant tool, library management system, android 5 easter egg and healthy programmer</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
